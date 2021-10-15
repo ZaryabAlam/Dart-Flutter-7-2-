1362021c2f65e48537cf8bdc13319ec355c2f54a
ZaryabAlam <zaryabalam97@gmail.com>
2021-10-15 21:40:44 +0500

    Work Done

---
+++ hello/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+// ignore: use_key_in_widget_constructors
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Text("Hello World"),
+      ),
+    );
+  }
+}
